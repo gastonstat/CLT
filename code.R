5e176bc6f8ec964f6568a9ebed7c337ac524cc61
Jazmine Gamboa <j.gamboa@berkeley.edu>
2024-04-09 17:37:19 -0700

    Added small, medium, large samples

--- code.R
+++ code.R
@@ -11,16 +11,26 @@ sigma = sd(X)
 
 # CLT
 # xbars follow a Normal distribution (mu, sigma/sqrt(n))
-sample_size = 30
-
+small_sample_size = 30
+medium_sample_size = 100
+large_sample_size = 1000
+sample_sizes = c(small_sample_size, medium_sample_size, large_sample_size)
 num_samples = 100
 
-xbars = matrix(0, ncol = num_samples)
+xbars = matrix(0, nrow = num_samples, ncol = 3)
 
-for (i in 1:num_samples) {
-  xbars[i] = mean(sample(X, size = sample_size))
+for (n in 1:3){
+  for (i in 1:num_samples) {
+    xbars[i,n] = mean(sample(X, size = sample_sizes[n]))
+  }
 }
 
 # distribution
-hist(xbars)
+op = par(mfrow = c(1,3))
+hist(xbars[,1], xlim = c(2500, 6500))
+hist(xbars[,2], xlim = c(2500, 6500))
+hist(xbars[,3], xlim = c(2500, 6500))
+par(op)
+
+
 
